e222e8a3dd820f826a877c0ea57d32c552ed8a22
Valentin <valent.chamboredon@gmail.com>
2025-03-05 15:51:46 +0100

    premier push

---
+++ src/app/composants/generations/generations.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generations',
+  imports: [],
+  templateUrl: './generations.component.html',
+  styleUrl: './generations.component.css'
+})
+export class GenerationsComponent {
+
+}
